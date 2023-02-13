a01da6b50352eb0be7b9395ea87f727a14ee552b
IrishAir <bls21@mail.ru>
2023-02-13 10:49:27 +0500

    prettier and eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import eslint from '@rollup/plugin-eslint'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		{ ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }), enforce: 'pre' },
+		react(),
+	],
 })
