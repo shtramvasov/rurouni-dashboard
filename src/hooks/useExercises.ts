942f3c88058e186c2b4f36f8c4d5eb63263b0eb7
IrishAir <bls21@mail.ru>
2023-02-26 09:45:21 +0500

    added useExerciseById hook

--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -4,3 +4,9 @@ import { ExercisesService } from '@services/exercises'
 export const useExercises = () => {
 	return useQuery('exercises', () => ExercisesService.fetchAllExercises())
 }
+
+export const useExerciseById = (id: string) => {
+	return useQuery(['exercise', id], () =>
+		ExercisesService.fetchExerciseById(id),
+	)
+}
--- src/pages/ExercisePage/ExercisePage.tsx
+++ src/pages/ExercisePage/ExercisePage.tsx
@@ -1,16 +1,13 @@
 import { FC } from 'react'
-import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import Layout from '@components/Layout/Layout'
 import Statistics from '@components/Statistics/Statistics'
-import { ExercisesService } from '@services/exercises'
+import { useExerciseById } from '@hooks/useExercises'
 import { getExercisesData } from './statistics.data'
 
 const ExercisePage: FC = () => {
 	const { id } = useParams()
-	const { data: exercise } = useQuery(['exercise', id], () =>
-		ExercisesService.fetchExerciseById(id as string),
-	)
+	const { data: exercise } = useExerciseById(id as string)
 	const data = getExercisesData(exercise?.data)
 
 	return (
